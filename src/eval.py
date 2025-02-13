3f21b902e6a115309eae6c9bc746bc0086f89460
yqtong <371516709@qq.com>
2025-02-13 19:25:40 +0800

    Update eval.py

--- src/eval.py
+++ src/eval.py
@@ -1,19 +1,3 @@
-# =========================================================================
-# Copyright (C) 2021. Huawei Technologies Co., Ltd. All rights reserved.
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# =========================================================================
-
 from typing import List, Tuple, Dict, Any
 
 import torch
@@ -96,8 +80,6 @@ def dev(model, dev_loader, device, out_path, epoch=0):
                                 dev_batch['hist_input_mask'].to(device),
                                 dev_batch['hist_mask'].to(device),
                                 dev_batch['hist_category_ids'].to(device),
-                                dev_batch['CTR'].to(device),
-                                dev_batch['recency'].to(device),
                                 dev_batch['template_ids'].to(device),
                                 dev_batch['template_token_type'].to(device),
                                 dev_batch['template_mask'].to(device),
